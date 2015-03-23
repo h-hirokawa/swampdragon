4530e0b4eb95bbc27e204d8dc26cf9ba4d698dbe
h-hirokawa <h.hirokawa@realglobe.jp>
2015-03-23 19:09:44 +0900

    Now SettingsHandler can treat X-Forwarded-Proto header.

--- swampdragon/settings_provider.py
+++ swampdragon/settings_provider.py
@@ -7,11 +7,13 @@ class SettingsHandler(RequestHandler):
         self.set_header("Content-Type", "application/javascript")
 
     def get(self, *args, **kwargs):
+        protocol = self.request.headers.get('X-Forwarded-Proto', self.request.protocol)
+
         data = '''window.swampdragon_settings = {settings};
 window.swampdragon_host = "{protocol}://{host}";
 '''.format(**{
             'settings': SwampDragonSettings().to_dict(),
             'host': self.request.headers['Host'],
-            'protocol': self.request.protocol,
+            'protocol': protocol,
         })
         self.write(data)
