ee6d55a9313eaf8b4a5053a5302854a4eaa09385
h-hirokawa <h.hirokawa@realglobe.jp>
2016-03-09 15:00:59 +0900

    Add X-Forwarded-Port support for to settings_provider.

--- swampdragon/settings_provider.py
+++ swampdragon/settings_provider.py
@@ -7,13 +7,14 @@ class SettingsHandler(RequestHandler):
         self.set_header("Content-Type", "application/javascript")
 
     def get(self, *args, **kwargs):
-        protocol = self.request.headers.get('X-Forwarded-Proto', self.request.protocol)
+        protocol = self.request.headers.get('X-Forwarded-Proto', self.request.protocol).lower()
 
         data = '''window.swampdragon_settings = {settings};
-window.swampdragon_host = "{protocol}://{host}";
+window.swampdragon_host = "{protocol}://{host}:{port}";
 '''.format(**{
             'settings': SwampDragonSettings().to_dict(),
             'host': self.request.headers['Host'],
+            'port': self.request.headers.get('X-Forwarded-Port', 443 if protocol == 'https' else 80),
             'protocol': protocol,
         })
         self.write(data)
